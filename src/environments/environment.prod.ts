fa979c9b3a0775c252bc08ad0463a38efea3a0b0
Miguel Rodriguez <miguel.rodriguez@rhinosystems.mx>
2018-09-13 11:14:28 -0500

    Ajusta archivos para deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,4 +5,16 @@
  */
 export const environment = {
   production: true,
+  ad: {
+    tenant: '5b2186b8-0f12-4122-90d9-d1517454250b',
+    clientId: '6d57b947-47cd-4eda-9e5a-e7afae515349',
+    redirectUri: window.location.origin,
+    crm: 'https://etherium0.api.crm.dynamics.com/api/data/v9.0/',
+    endpoints: {
+      'crm' : 'https://etherium0.api.crm.dynamics.com'
+    },
+    navigateToLoginRequestUrl: false,
+    cacheLocation: 'localStorage',
+    resource: 'https://etherium0.api.crm.dynamics.com'
+  }
 };
